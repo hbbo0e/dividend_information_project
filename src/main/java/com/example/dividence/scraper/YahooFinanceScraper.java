507e875dc0421bf6ae7863234ded35db9cdc3366
hbbo0e <44goodluck44@gmail.com>
2025-09-11 19:09:46 +0900

    feat: #1 배당금 정보 스크래핑

---
+++ src/main/java/com/example/dividence/scraper/YahooFinanceScraper.java
@@ -0,0 +1,65 @@
+package com.example.dividence.scraper;
+
+import com.example.dividence.model.Company;
+import com.example.dividence.model.Dividend;
+import com.example.dividence.model.ScrapedResult;
+import com.example.dividence.model.constants.Month;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class YahooFinanceScraper {
+
+  private static final String STATISTICS_URL = "https://finance.yahoo.com/quote/%s/history?period1=%d&period2=%d&interval=1mo";
+  //private static final long START_TIME = 86400; // 1970 (너무 과거) 타임 아웃 가능성
+  static long now = System.currentTimeMillis() / 1000;
+  private static final long START_TIME = now - 31536000;
+
+  public ScrapedResult scrap (Company company) {
+    var scrapResult = new ScrapedResult();
+    scrapResult.setCompany(company);
+
+    try {
+      String url = String.format(STATISTICS_URL, company.getTicker(), START_TIME, now);
+
+      Connection connection = Jsoup.connect(url)
+          .userAgent(
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36");
+      Document document = connection.get();
+      Elements dividendRows = document.select("tr:has(td.event:contains(Dividend))");
+
+      List<Dividend> dividends = new ArrayList<>();
+      for (Element row : dividendRows) {
+        String[] date = row.select("td").first().text().split("[, ]+");
+        int month = Month.strToNumber(date[0]);
+        int day = Integer.parseInt(date[1]);
+        int year = Integer.parseInt(date[2]);
+        String dividendText = row.select("td.event").text();
+
+        if (month < 0){
+          throw new RuntimeException("unexpected Month enum value -> " + date[0]);
+        }
+
+        dividends.add(Dividend.builder()
+                            .date(LocalDateTime.of(year, month, day, 0, 0))
+                            .dividend(dividendText)
+                            .build());
+      }
+      scrapResult.setDividendEntities(dividends);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return scrapResult;
+  }
+
+  public Company scrapCompanyByTicker(String ticker){
+    return null;
+  }
+}
