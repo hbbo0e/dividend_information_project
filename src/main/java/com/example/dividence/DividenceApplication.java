bbfae91434b3b716253be170b85634e2f04b5526
hbbo0e <44goodluck44@gmail.com>
2025-09-12 22:54:16 +0900

    config: #1 redis cache config 설정

--- src/main/java/com/example/dividence/DividenceApplication.java
+++ src/main/java/com/example/dividence/DividenceApplication.java
@@ -3,10 +3,12 @@ import com.example.dividence.model.Company;
 import com.example.dividence.scraper.YahooFinanceScraper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class DividenceApplication {
   public static void main(String[] args) {
     SpringApplication.run(DividenceApplication.class, args);
