8be26230fe19d01385b52f7493f700432fcb61d6
hbbo0e <44goodluck44@gmail.com>
2025-09-14 16:13:42 +0900

    feat: #1 log.info 추가

--- src/main/java/com/example/dividence/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/dividence/security/JwtAuthenticationFilter.java
@@ -30,11 +30,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
       throws ServletException, IOException {
       String token = this.resolveTokenFromRequest(request);
 
-        if (StringUtils.hasText(token) && this.tokenProvider.validateToken(token)) {
-            Authentication auth = this.tokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(auth);
-  }
-        filterChain.doFilter(request, response);
+      if (StringUtils.hasText(token) && this.tokenProvider.validateToken(token)) {
+        Authentication auth = this.tokenProvider.getAuthentication(token);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        log.info(String.format("[%s] -> %s", this.tokenProvider.getUsername(token), request.getRequestURI()));
+      }
+      filterChain.doFilter(request, response);
   }
 
   private String resolveTokenFromRequest(HttpServletRequest request){
